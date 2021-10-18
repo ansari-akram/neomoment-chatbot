de7107552feee8dbcc047dbe250e98465a67e10f
shaikhsaalem1997 <48882096+shaikhsaalem1997@users.noreply.github.com>
2021-10-18 22:40:01 +0530

    Update custom.js

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,3 +2,17 @@ function myTest(text, voiceOf) {
     responsiveVoice.speak(text, voiceOf, {rate: 0.9});
     console.log('Getting voice output!')
 }
+
+function getLocalStream() {
+    navigator.mediaDevices.getUserMedia({video: false, audio: true}).then( stream => {
+        window.localAudio.autoplay = false;
+    }).catch( err => {
+        if(err === PERMISSION_DENIED) {
+            // Explain why you need permission and how to update the permission setting
+            console.log("u got an error:" + err)
+            }
+    });
+}
+
+
+getLocalStream();
